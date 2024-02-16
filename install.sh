7c18d608d6eb9cdecadf58142496c116565cfcbd
den0620 <denis2006d@gmail.com>
2024-02-16 18:17:57 +0500

    Make install.sh work again

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 [[ ! -d "build" ]] && mkdir build
 cd build
 
-gcc $(pkg-config --cflags gtksourceview-5) -o lampanel-gtk ../lampanel-gtk.c $(pkg-config --libs gtksourceview-5)
+gcc $(pkg-config --cflags gtksourceview-5) -o lampanel-gtk ../src/lampanel-gtk.c $(pkg-config --libs gtksourceview-5)
 
 echo "+------+----------------------------------------+"
 echo "| Done | Executable now should be in ./lampanel |"
